0afeec89f0e56bd0ae91efc5c73c6765e8a1356d
zdresser <42179244+zdresser@users.noreply.github.com>
2022-02-14 21:24:33 -0500

    Types

--- types/types.ts
+++ types/types.ts
@@ -1,7 +1,13 @@
+import { DefaultSession } from 'next-auth'
+
 interface FileReaderEventTarget extends EventTarget {
   result: string
 }
 
 export interface IFileReaderEvent extends ProgressEvent {
   target: FileReaderEventTarget
+}
+
+export interface IEmoji {
+  native: string
 }
